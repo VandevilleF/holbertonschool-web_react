c10bbe516d4a2fb6a69483e0fa2d73b62778093e
VandevilleF <flo.vandeville@gmail.com>
2025-04-15 16:11:22 +0200

    Append paragraphe to body one by one

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from "jquery";
 
-const p1 = $('<p></p>').text('Holberton Dashboard');
-const p2 = $('<p></p>').text('Dashboard data for the students');
-const p3 = $('<p></p>').text('Copyright - Holberton School');
-
-$('body').append(p1, p2, p3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
