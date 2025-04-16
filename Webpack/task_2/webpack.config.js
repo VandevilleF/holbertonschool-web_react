fd274c54b99b05fdb46fedda74c595fc02567070
VandevilleF <flo.vandeville@gmail.com>
2025-04-16 11:36:08 +0200

    Task 2, local test OK

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
