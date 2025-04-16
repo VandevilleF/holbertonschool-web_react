6987da949c62e9709e17dac766e1c185934b00af
VandevilleF <flo.vandeville@gmail.com>
2025-04-16 16:51:19 +0200

    Try to fix task 3

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,57 +1,37 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: {
     header: './modules/header/header.js',
     body: './modules/body/body.js',
     footer: './modules/footer/footer.js',
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "public"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin(),
   ],
-  // Config for server developpement
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
+    static: './dist',
     port: 8564,
     open: true,
-    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'assets/',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: { progressive: true },
-              optipng: { enabled: true },
-              pngquant: { quality: [0.65, 0.90], speed: 4 },
-              gifsicle: { interlaced: false },
-            },
-          },
-        ],
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
