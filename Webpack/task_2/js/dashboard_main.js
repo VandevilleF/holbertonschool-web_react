b302b4c6ba79c7c7847f7e0da1a00760218c8d57
VandevilleF <flo.vandeville@gmail.com>
2025-04-16 11:37:52 +0200

    Change order of import

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,6 +1,6 @@
+import '../css/main.css';
 import $ from "jquery";
 import _ from "lodash";
-import '../css/main.css';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
