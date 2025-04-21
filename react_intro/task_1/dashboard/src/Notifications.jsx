a58aefb8f939549d1de8977561d39244bd54dab1
VandevilleF <flo.vandeville@gmail.com>
2025-04-21 18:35:24 +0200

    Task 2, local test OK

---
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -0,0 +1,12 @@
+import './Notifications.css'
+export function Notifications() {
+  return (
+    <>
+      <div className="notifications">
+        <p>
+        Here is the list of notifications
+        </p>
+      </div>
+    </>
+  )
+}
