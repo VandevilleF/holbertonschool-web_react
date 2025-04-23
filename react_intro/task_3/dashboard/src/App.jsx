222863c5183fe33e6d9611d4ae1feecb61623422
VandevilleF <flo.vandeville@gmail.com>
2025-04-23 14:14:44 +0200

    Task 6, local test OK

---
+++ react_intro/task_3/dashboard/src/App.jsx
@@ -0,0 +1,41 @@
+import holbertonLogo from './assets/holberton-logo.jpg'
+import './App.css'
+import { getCurrentYear, getFooterCopy } from './utils'
+import { Notifications } from './Notifications'
+
+function App() {
+  return (
+    <>
+      <div className='root-notifications'>
+        <Notifications />
+      </div>
+      <div className='App-header'>
+        <img src={holbertonLogo} className="logo" alt="holberton logo" />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>
+        Login to access the full dashboard
+        </p>
+        <label htmlFor="email">
+          Email:
+          <input id="email" type="email" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input id="password" type="password" />
+        </label>
+        <button>
+          OK
+        </button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright { getCurrentYear() } - { getFooterCopy() }
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
