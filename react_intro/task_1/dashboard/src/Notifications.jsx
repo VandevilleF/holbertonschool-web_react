076c54bed0c08bfdd92403226a786002c8efb6fc
VandevilleF <flo.vandeville@gmail.com>
2025-04-24 13:02:44 +0200

    Change form of p tag

--- react_intro/task_1/dashboard/src/Notifications.jsx
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -2,9 +2,7 @@ import './Notifications.css'
 export function Notifications() {
   return (
     <div className="notifications">
-      <p>
-      Here is the list of notifications
-      </p>
+      <p>Here is the list of notifications</p>
     </div>
 )
 }
