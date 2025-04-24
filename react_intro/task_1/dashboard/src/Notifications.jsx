28543fb5509446158ef884bbd7ad495b7e604ba0
VandevilleF <flo.vandeville@gmail.com>
2025-04-24 13:14:34 +0200

    rework return of Notifications component

--- react_intro/task_1/dashboard/src/Notifications.jsx
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -1,8 +1,10 @@
 import './Notifications.css'
+import React from 'react'
+
 export function Notifications() {
   return (
-    <div className="notifications">
-      <p>Here is the list of notifications</p>
-    </div>
-)
+  <div className="notifications">
+    <p>Here is the list of notifications</p>
+  </div>
+  )
 }
