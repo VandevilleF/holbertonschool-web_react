54db087e73a8507dd4928114db4fab7dd873fc57
VandevilleF <flo.vandeville@gmail.com>
2025-05-05 16:27:30 +0200

    use only one test

--- react_component/task_1/dashboard/src/App/App.spec.js
+++ react_component/task_1/dashboard/src/App/App.spec.js
@@ -5,33 +5,21 @@ test('App component', () => {
   render(<App />);
 });
 
-test('Display alert when Ctrl + H si press', () => {
-  // Spy alert and mock alert popup
-  const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
-
-  render(<App />);
-
-  // Simulate the keydown event (Ctrl+h)
-  fireEvent.keyDown(window, { key: 'h', ctrlKey: true });
-
-  expect(alertSpy).toHaveBeenCalledWith('Logging you out');
-
-  // Restore alert after test
-  alertSpy.mockRestore();
-});
-
 test('should call logOut function when ctrl+h is pressed', () => {
   // Create a mock function for logOut prop
   const logOutMock = jest.fn();
-
+  // Spy alert and mock alert popup
   const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
   // Render the component with the mock logOut function
   render(<App logOut={logOutMock} />);
 
+  // Simulate the keydown event (Ctrl+h)
   fireEvent.keyDown(window, { key: 'h', ctrlKey: true });
 
+  expect(alertSpy).toHaveBeenCalledWith('Logging you out');
   expect(logOutMock).toHaveBeenCalledTimes(1);
 
+  // Restore alert after test
   alertSpy.mockRestore();
 });
