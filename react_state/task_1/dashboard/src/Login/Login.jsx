73312a047e6735e2ef1b8776e5b444b23fe3ce89
VandevilleF <flo.vandeville@gmail.com>
2025-05-19 16:25:51 +0200

    Task 1, local test OK

---
+++ react_state/task_1/dashboard/src/Login/Login.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import WithLogging from '../HOC/WithLogging'
+import { StyleSheet, css } from 'aphrodite';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            email: '',
+            password: '',
+            enableSubmit: false,
+        }
+        this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    }
+
+    handleLoginSubmit = () => {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleChangeEmail = (event) => {
+        const email = event.target.value;
+        const { password } = this.state;
+        const enableSubmit = this.validateForm(email, password);
+        this.setState({ email, enableSubmit });
+  };
+
+    handleChangePassword = (event) => {
+        const password = event.target.value;
+        const { email } = this.state;
+        const enableSubmit = this.validateForm(email, password);
+        this.setState({ password, enableSubmit });
+  };
+
+    validateForm = (email, password) => {
+        const isValidEmail = /\S+@\S+\.\S+/.test(email);
+        const isValidPassword = password.length >= 8;
+        return email !== '' && password !== '' && isValidEmail && isValidPassword;
+    };
+
+    render() {
+        const { email, password, enableSubmit } = this.state;
+
+        return (
+        <div className={css(styles.bodystyle)}>
+            <p>Login to access the full dashboard</p>
+            <form onSubmit={this.handleLoginSubmit}>
+                <label htmlFor="email">Email:
+                    <input className={css(styles.bodyinput)} id="email" type="email" value={email} onChange={this.handleChangeEmail} />
+                </label>
+                <label htmlFor="password">Password:
+                    <input className={css(styles.bodyinput)} id="password" type="password" value={password} onChange={this.handleChangePassword} />
+                </label>
+                <input className={styles.button} type='submit' value='OK' onSubmit={this.handleLoginSubmit} disabled={!enableSubmit}/>
+            </form>
+        </div>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    bodystyle: {
+        display: 'block',
+        justifyContent: 'flex-start',
+        padding: '0.5rem',
+        flex: '1',
+        '@media (max-width: 900px)': {
+            display:' flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+        },
+    },
+    bodyinput: {
+        margin: '0 0.5rem 0',
+        '@media (max-width: 900px)': {
+            border: 'none',
+            outline: 'none',
+        }
+    },
+    button: {
+        marginTop: '1rem',
+        display: 'inline-block',
+        width: 'auto',
+        '@media (max-width: 900px)': {
+            width: 'fit-content',
+        },
+    },
+})
+
+const LoginWithLogging = WithLogging(Login);
+export default LoginWithLogging;
