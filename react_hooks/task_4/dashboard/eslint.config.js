85b6f045e51dd168f46709635b4bd8d486bb9c1e
VandevilleF <flo.vandeville@gmail.com>
2025-05-27 15:05:53 +0200

    Add parametre to take process valid

--- react_hooks/task_4/dashboard/eslint.config.js
+++ react_hooks/task_4/dashboard/eslint.config.js
@@ -12,6 +12,7 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.jest,
+        process: 'readonly',
       },
       parserOptions: {
         ecmaVersion: 'latest',
