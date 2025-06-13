3afebe12bb2e831ab6e24d45706de62a10fb21d5
VandevilleF <flo.vandeville@gmail.com>
2025-06-13 10:08:57 +0200

    Refactor: Comment out unused styles in CourseListRow component for clarity

--- react_redux-part1/task_5/dashboard/src/pages/CourseList/CourseListRow/CourseListRow.jsx
+++ react_redux-part1/task_5/dashboard/src/pages/CourseList/CourseListRow/CourseListRow.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
-function CourseListRow({ isHeader, textFirstCell, textSecondCell, style }) {
-  const rowStyle = css(isHeader ? styles.headerRow : styles.dataRow);
-  const cellStyle = css(styles.cell);
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  // const rowStyle = css(isHeader ? styles.headerRow : styles.dataRow);
+  // const cellStyle = css(styles.cell);
   if (isHeader) {
     if (textSecondCell === null || textSecondCell === undefined) {
       return (
-        <tr className={css(styles.headerRow)}>
-          <th className={cellStyle} colSpan="2">{textFirstCell}</th>
+        <tr
+        // className={css(styles.headerRow)}
+        >
+          <th
+          // className={cellStyle}
+          colSpan="2">{textFirstCell}</th>
         </tr>
       );
     } else {
       return (
-        <tr className={css(styles.headerRow)}>
-          <th className={cellStyle}>{textFirstCell}</th>
-          <th className={cellStyle}>{textSecondCell}</th>
+        <tr
+        // className={css(styles.headerRow)}
+        >
+          <th
+          // className={cellStyle}
+          >{textFirstCell}</th>
+          <th
+          // className={cellStyle}
+          >{textSecondCell}</th>
         </tr>
       );
     }
   } else {
     return (
-      <tr className={css(styles.dataRow)}>
-        <td className={cellStyle}>{textFirstCell}</td>
-        <td className={cellStyle}>{textSecondCell}</td>
+      <tr
+      // className={css(styles.dataRow)}
+      >
+        <td
+        // className={cellStyle}
+        >{textFirstCell}</td>
+        <td
+        // className={cellStyle}
+        >{textSecondCell}</td>
       </tr>
     );
   }
